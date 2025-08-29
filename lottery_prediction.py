ef187e069631c90a9feea4f19c16b9ae49d2c852
Joker7822 <107754027+Joker7822@users.noreply.github.com>
2025-08-29 20:23:13 +0900

    Update lottery_prediction.py

--- lottery_prediction.py
+++ lottery_prediction.py
@@ -160,6 +160,7 @@ from itertools import combinations
 import shutil
 import traceback
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 import tensorflow as tf
 from gymnasium.utils import seeding
 import time
